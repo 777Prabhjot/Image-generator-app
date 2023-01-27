f601d3db762a9374661a67188c3fcf5516ccfe55
Prabhjot <777prabhjot@gmail.com>
2023-01-27 12:36:58 +0530

    image generator app

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const ejs = require('ejs');
+const dotenv = require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+
+
+const configuration = new Configuration({
+    apiKey: process.env.ApiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+let url = "";
+
+app.get('/', (req, res) => {
+    res.render("home.ejs", {url: url})
+})
+
+app.post('/generateImage', async (req, res) => {
+    const { image } = req.body;
+    try {
+        const response = await openai.createImage({
+            prompt: image,
+            n: 1,
+            size: "512x512",
+          });
+         const image_url = response.data.data[0].url;
+          url = image_url;
+         return res.status(200).json({url: image_url});
+        
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+    }
+})
+
+app.listen(5000, () => {
+    console.log('server is running');
+})
